b30e65b4cf7dfc98a932cb030a995976fb728210
jakubklimczak <kubaklim2001@gmail.com>
2024-06-08 18:37:05 +0200

    dotnet8.0 setup

--- ConvexHullApp/ConvexHullApp/MainWindow.xaml.cs
+++ ConvexHullApp/ConvexHullApp/MainWindow.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -13,16 +9,15 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ConvexHullApp
+namespace ConvexHullApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Convex_Hull
+# Convex Hull
+
+This project is created for the first semester of master's degree subject 'Computational geometry'.
+
+It is written in C# and uses WPF for graphical UI.
+
+The aim of the project is to allow comparison between different convex hull algorithms and measure the time of their execution. 
+
+It is possible to choose or randomise the vertices and view the visualisation of the results.
