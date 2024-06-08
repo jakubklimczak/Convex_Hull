5a81d5e07618c11496a802c21aa911db6da64a28
jakubklimczak <kubaklim2001@gmail.com>
2024-06-08 19:24:36 +0200

    progress on Jarvis-Hull

---
+++ ConvexHullApp/ConvexHullApp/ConvexHullAlgorithms.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvexHullApp
+{
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    public class Result
+    {
+        public Point[] Points;
+
+        public Result(Point[] input)
+        {
+            Points = input;
+        }
+
+
+    }
+
+    internal class ConvexHullAlgorithms
+    {
+        public ConvexHullAlgorithms() { }
+
+        /*
+         * Function that performs Jarvis-Hull algorithm on an array of 2d geometrical points (X, Y)
+         * Returns an ordered array of points
+         */
+        public Result JarvisHullAlgorithm(Point[] inputPointsArray)
+        {
+            if (inputPointsArray.Length == 0 || inputPointsArray.Length == 1 || inputPointsArray.Length == 2)
+                { return new Result(inputPointsArray); }
+
+
+        }
+    }
+}
