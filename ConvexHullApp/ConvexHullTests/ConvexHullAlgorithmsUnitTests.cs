76be7bd3ce6e4860f81187d85dff243f3e2f3d45
Jakub Klimczak <kubaklim2001@gmail.com>
2024-06-20 21:59:06 +0200

    Fixed Jarvis Hull algorithm, linter suggestions and it is now called by the GUI (#3)

--- ConvexHullApp/ConvexHullTests/ConvexHullAlgorithmsUnitTests.cs
+++ ConvexHullApp/ConvexHullTests/ConvexHullAlgorithmsUnitTests.cs
@@ -6,9 +6,9 @@
         [Test]
         public void TestOrientationCollinear()
         {
-            var p = new Point { X = 0, Y = 0 };
-            var q = new Point { X = 1, Y = 1 };
-            var r = new Point { X = 2, Y = 2 };
+            var p = new Point(0, 0);
+            var q = new Point (1, 1);
+            var r = new Point (2, 2);
             var result = ConvexHullAlgorithms.Orientation(p, q, r);
             Assert.That(result, Is.EqualTo(0));
         }
@@ -16,31 +16,21 @@
         [Test]
         public void TestOrientationClockwise()
         {
-            var p = new Point { X = 0, Y = 0 };
-            var q = new Point { X = 4, Y = 4 };
-            var r = new Point { X = 1, Y = 2 };
-            var result = ConvexHullAlgorithms.Orientation(p, q, r);
-            Assert.That(result, Is.EqualTo(2));
-        }
-
-        [Test]
-        public void TestOrientationAntiClockwise()
-        {
-            var p = new Point { X = 0, Y = 0 };
-            var q = new Point { X = 1, Y = 1 };
-            var r = new Point { X = 2, Y = 0 };
+            var p = new Point (0, 0);
+            var q = new Point (4, 4);
+            var r = new Point (1, 2);
             var result = ConvexHullAlgorithms.Orientation(p, q, r);
             Assert.That(result, Is.EqualTo(1));
         }
 
         [Test]
-        public void TestDistance()
+        public void TestOrientationAntiClockwise()
         {
-            // 3-4-5 triangle, distance is 5
-            var p = new Point { X = 0, Y = 0 };
-            var q = new Point { X = 3, Y = 4 };
-            var result = ConvexHullAlgorithms.Distance(p, q);
-            Assert.That(result, Is.EqualTo(5.0));
+            var p = new Point (0, 0);
+            var q = new Point (1, 1);
+            var r = new Point (2, 0);
+            var result = ConvexHullAlgorithms.Orientation(p, q, r);
+            Assert.That(result, Is.EqualTo(-1));
         }
 
         [Test]
@@ -48,16 +38,16 @@
         {
             var points = new[]
             {
-                new Point { X = 0, Y = 0 },
-                new Point { X = 1, Y = 1 },
-                new Point { X = 2, Y = 0 }
+                new Point (0, 0),
+                new Point (1, 1),
+                new Point (2, 0)
             };
 
             var result = ConvexHullAlgorithms.JarvisHullAlgorithm(points);
             Assert.Multiple(() =>
             {
                 Assert.That(result.Points, Has.Length.EqualTo(3));
-                Assert.That(result.Shape, Is.EqualTo(GeometricalShape.Triangle));
+                Assert.That(result.Shape, Is.EqualTo("Triangle"));
             });
         }
 
@@ -66,15 +56,14 @@
         {
             var points = new[]
             {
-                new Point { X = 0, Y = 0 },
-                new Point { X = 1, Y = 1 },
-            };
+                new Point (0, 0),
+                new Point (1, 1)     };
 
             var result = ConvexHullAlgorithms.JarvisHullAlgorithm(points);
             Assert.Multiple(() =>
             {
                 Assert.That(result.Points, Has.Length.EqualTo(2));
-                Assert.That(result.Shape, Is.EqualTo(GeometricalShape.Line));
+                Assert.That(result.Shape, Is.EqualTo("Line Segment - Convex Hull invalid"));
             });
         }
 
@@ -83,26 +72,26 @@
         {
             var points = new[]
             {
-                new Point { X = 0, Y = 3 },
-                new Point { X = 2, Y = 2 },
-                new Point { X = 1, Y = 1 },
-                new Point { X = 2, Y = 1 },
-                new Point { X = 3, Y = 0 },
-                new Point { X = 0, Y = 0 },
-                new Point { X = 3, Y = 3 }
+                new Point (0, 3),
+                new Point (2, 2),
+                new Point (1, 1),
+                new Point (2, 1),
+                new Point (3, 0),
+                new Point (0, 0),
+                new Point (3, 3)
             };
 
             var result = ConvexHullAlgorithms.JarvisHullAlgorithm(points);
             Assert.Multiple(() =>
             {
                 Assert.That(result.Points, Has.Length.EqualTo(4));
-                Assert.That(result.Shape, Is.EqualTo(GeometricalShape.Quadrilateral));
+                Assert.That(result.Shape, Is.EqualTo("Quadrilateral"));
                 Assert.That(result.Points, Is.EquivalentTo(new[]
                 {
-                    new Point { X = 0, Y = 3 },
-                    new Point { X = 3, Y = 3 },
-                    new Point { X = 3, Y = 0 },
-                    new Point { X = 0, Y = 0 }
+                    new Point (0, 3),
+                    new Point (3, 3),
+                    new Point (3, 0),
+                    new Point (0, 0)
                 }));
             });
         }
@@ -112,12 +101,12 @@
         {
             var points = new[]
             {
-                new Point { X = 0, Y = 0 },
-                new Point { X = 0, Y = 0 },
-                new Point { X = 1, Y = 1 },
-                new Point { X = 1, Y = 1 },
-                new Point { X = 2, Y = 0 },
-                new Point { X = 2, Y = 0 }
+                new Point (0, 0),
+                new Point (0, 0),
+                new Point (1, 1),
+                new Point (1, 1),
+                new Point (2, 0),
+                new Point (2, 0)
             };
 
             var result = ConvexHullAlgorithms.JarvisHullAlgorithm(points);
@@ -125,12 +114,12 @@
             Assert.Multiple(() =>
             {
                 Assert.That(result.Points, Has.Length.EqualTo(3));
-                Assert.That(result.Shape, Is.EqualTo(GeometricalShape.Triangle));
+                Assert.That(result.Shape, Is.EqualTo("Triangle"));
                 Assert.That(result.Points, Is.EquivalentTo(new[]
                 {
-                    new Point { X = 0, Y = 0 },
-                    new Point { X = 2, Y = 0 },
-                    new Point { X = 1, Y = 1 }
+                    new Point (0, 0),
+                    new Point (2, 0),
+                    new Point (1, 1)
                 }));
             });
         }
@@ -140,7 +129,7 @@
         {
             var points = new[]
             {
-                new Point { X = 0, Y = 0 }
+                new Point (0, 0)
             };
 
             var result = ConvexHullAlgorithms.JarvisHullAlgorithm(points);
@@ -148,7 +137,7 @@
             Assert.Multiple(() =>
             {
                 Assert.That(result.Points, Has.Length.EqualTo(1));
-                Assert.That(result.Shape, Is.EqualTo(GeometricalShape.Point));
+                Assert.That(result.Shape, Is.EqualTo("Point - Convex Hull invalid"));
             });
         }
 
@@ -162,8 +151,41 @@
             Assert.Multiple(() =>
             {
                 Assert.That(result.Points, Has.Length.EqualTo(0));
-                Assert.That(result.Shape, Is.EqualTo(GeometricalShape.BlankSpace));
+                Assert.That(result.Shape, Is.EqualTo("Blank - Convex Hull invalid"));
             });
+        }
+
+        [Test]
+        public void TestRemoveDuplicates()
+        {
+            var points = new[]
+{
+                new Point (0, 0),
+                new Point (0, 0),
+                new Point (1, 1),
+                new Point (1, 1),
+                new Point (2, 0),
+                new Point (2, 0),
+                new Point (2, 1),
+                new Point (0, 0)
+            };
+
+            var result = ConvexHullAlgorithms.RemoveDuplicates(points);
+
+            Assert.Multiple(
+                () =>
+                {
+                    Assert.That(result, Has.Length.EqualTo(4));
+                    Assert.That(result, Is.EquivalentTo(new[]
+                    {
+                        new Point (0, 0),
+                        new Point (2, 0),
+                        new Point (1, 1),
+                        new Point (2, 1),
+                    })
+                    );
+                }
+            );
         }
     }
 }
